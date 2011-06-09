fd9b1976a2a12a4f8337ebc183def4d346ff22db
Patrick F. Marques <patrickfmarques AT av DOT it DOT pt>
2011-06-09 14:10:03 +0100

    Bugs correction.
     * wrap messages
     * correct logcat options
     * correct color of errors messages

--- coloredlogcat.pl
+++ coloredlogcat.pl
@@ -7,51 +7,70 @@
 use warnings;
 use strict;
 use Term::ANSIColor 3.0;
+use Term::ReadKey;
+use Text::Wrap;
 
+# Wrap on any character
+$Text::Wrap::break="[.]";
+
+# output configuration
 my $confs;
 
-#PID confs
+# PID confs
 ${$confs}{'PID' }{'COLOR'} = 'black on_bright_black';
 ${$confs}{'PID' }{'SPACE'} = 6;
-#Priority confs
+# Priority confs
 ${$confs}{'PRIO' }{'V'} = 'black on_white';
 ${$confs}{'PRIO' }{'D'} = 'black on_blue';
 ${$confs}{'PRIO' }{'I'} = 'black on_green';
 ${$confs}{'PRIO' }{'W'} = 'black on_yellow';
 ${$confs}{'PRIO' }{'E'} = 'black on_red';
 ${$confs}{'PRIO'}{'SPACE'} = 2;
-#Tag confs
+# Tag confs
 ${$confs}{'TAG' }{'COLOR'}{'dalvikvm'} = 'blue';
 ${$confs}{'TAG' }{'COLOR'}{'Process'} = 'blue';
 ${$confs}{'TAG' }{'COLOR'}{'ActivityManager'} = 'cyan';
 ${$confs}{'TAG' }{'COLOR'}{'ActivityThread'} = 'cyan';
 ${$confs}{'TAG' }{'SPACE'} = 20;
-#Message confs
+# Message confs
 ${$confs}{'MSG' }{'COLOR'}{'DEFAULT'} = '';
 ${$confs}{'MSG' }{'COLOR'}{'E'} = 'red';
 ${$confs}{'MSG' }{'SPACE'} = 0;
 
+# Circular colors to undifined tags
 my @colors = qw(red green yellow blue magenta cyan white);
 my $ops;
+my $columns;
+my $rows;
 
-if(-t STDIN) { # No stdin try execut adb logcat
-	my $pid = open(STDIN, "adb logcat $ops |") or die ("catn open adb logcat with this options $_");
+# Terminal size in characters
+($columns, $rows) = GetTerminalSize();
+
+# Max size of messages
+my $header = ${$confs}{'PID' }{'SPACE'} + ${$confs}{'PRIO'}{'SPACE'} + ${$confs}{'TAG' }{'SPACE'} + 4;
+
+# Break line at this position
+$Text::Wrap::columns= $columns - $header;
+
+if(-t STDIN) {
+	# No stdin try execut adb logcat
 	if( $#ARGV > 0 ) {
 		$ops = join(' ',@ARGV);
 	}
-} else { # Read from stdin
+	open(STDIN, "adb $ops logcat |") or die ("catn open adb logcat with options '$ops'");
+} else {
+	# Read from stdin
 }
 
 
 while (<STDIN>) {
-#while( <> ) {
 	my $prio;
 	my $tag;
 	my $pid;
 	my $msg;
+	my $color;
 	
-	#/(.)\/(.*)\(\s*(\d*)\s*\):(.*)/ or die ("logcat format is unknown...\n'$_'\n");
-	/^([A-Z])\/([^\(]+)\(([^\)]+)\): (.*)$/ or die ("logcat format is unknown...\n'$_'\n");
+	/^([VDIWE])\/([^\(]+)\(([^\)]+)\):(.*)$/ or die ("logcat format is unknown...\n'$_'\n");
 
 	$prio = $1;
 	$tag = $2;
@@ -68,14 +87,14 @@ while (<STDIN>) {
 	}
 
 	print colored( sprintf( "%*.*s ", ${$confs}{'TAG' }{'SPACE'}, ${$confs}{'TAG' }{'SPACE'}, $tag ), ${$confs}{'TAG' }{'COLOR'}{$tag} );
-	                                                              
+
 	print colored( sprintf( "%*.*s ", ${$confs}{'PRIO'}{'SPACE'}, ${$confs}{'PRIO'}{'SPACE'}, $prio), ${$confs}{'PRIO'}{$prio} );
 
-	# Hilight text messages
+	# Highlight text messages of errors
 	if( exists ${$confs}{'MSG' }{'COLOR'}{$prio} ) {
-		print colored( sprintf( " %*.*s", ${$confs}{'MSG' }{'SPACE'}, ${$confs}{'MSG' }{'SPACE'}, $msg ), ${$confs}{'MSG' }{'COLOR'}{$prio} );
-	} else {                                                          
-		print colored( sprintf( " %s", $msg ), ${$confs}{'MSG' }{'COLOR'}{'DEFAULT'} );
+		$color = ${$confs}{'MSG' }{'COLOR'}{$prio};
+	} else {
+		$color = ${$confs}{'MSG' }{'COLOR'}{'DEFAULT'};
 	}
-	print "\n";
+	print colored( sprintf( "%s\n", wrap("", " " x $header, $msg) ), $color);
 }
