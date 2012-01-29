85361a8fe213ed4e39b43f1e86a9d57418cdf88b
Patrick F. Marques <patrickfmarques@gmail.com>
2012-01-29 20:17:09 +0000

    Rename Helpers to Python and create a python module

---
+++ Python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PersonalUtils',
+  version='1.0',
+  description='Some useful scripts',
+  author='Patrick F. Marques',
+  author_email='patrick.marques at gmail dot com',
+  license='MIT',
+  packages=['Helpers', 'Logging']
+)
